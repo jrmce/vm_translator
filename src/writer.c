6bc3d7db700f404b8457d3a54e3f8fe0131d65b1
JR McEntee <jrmce12@gmail.com>
2021-11-27 09:27:11 -0500

    basic mem

--- src/writer.c
+++ src/writer.c
@@ -34,6 +34,31 @@ void write_command(char *command) {
   }
 }
 
+void push_basic_mem(Code *code, char *mem_loc) {
+  fprintf(fp_out, "%s\n", mem_loc);
+  int arg_2 = atoi(code->arg_2);
+  write_command("A=M\n");
+  if (arg_2 > 0) {
+    for (int i = 1; i <= arg_2; i++) {
+      write_command("A=A+1\n");
+    }
+  }
+  write_command("D=M @SP A=M M=D @SP M=M+1\n");
+}
+
+void pop_basic_mem(Code *code, char *mem_loc) {
+  write_command("@SP M=M-1 A=M D=M\n");
+  fprintf(fp_out, "%s\n", mem_loc);
+  int arg_2 = atoi(code->arg_2);
+  write_command("A=M\n");
+  if (arg_2 > 0) {
+    for (int i = 1; i <= arg_2; i++) {
+      write_command("A=A+1\n");
+    }
+  }
+  write_command("M=D @SP M=M+1\n");
+}
+
 void write_arithmetic(Code *code) {
   arithmetic_counter++;
 
@@ -83,9 +108,40 @@ void write_arithmetic(Code *code) {
 }
 
 void write_push_pop(Code *code) {
-  if (strcmp(code->arg_1, "constant") == 0) {
-    fprintf(fp_out, "@%s\n", code->arg_2);
-    write_command("D=A @SP A=M M=D @SP M=M+1\n");
+  if (code->command_type == PUSH) {
+    if (strcmp(code->arg_1, "constant") == 0) {
+      fprintf(fp_out, "@%s\n", code->arg_2);
+      write_command("D=A @SP A=M M=D @SP M=M+1\n");
+    } else if (strcmp(code->arg_1, "local") == 0) {
+      push_basic_mem(code, "@LCL");
+    } else if (strcmp(code->arg_1, "argument") == 0) {
+      push_basic_mem(code, "@ARG");
+    } else if (strcmp(code->arg_1, "this") == 0) {
+      push_basic_mem(code, "@THIS");
+    } else if (strcmp(code->arg_1, "that") == 0) {
+      push_basic_mem(code, "@THAT");
+    } else if (strcmp(code->arg_1, "temp") == 0) {
+      char location[7] = "@Temp";
+      strncat(location, code->arg_2, 2);
+      fprintf(fp_out, "%s\n", location);
+      write_command("A=M D=M @SP A=M M=D @SP M=M+1\n");
+    }
+  } else if (code->command_type == POP) {
+    if (strcmp(code->arg_1, "local") == 0) {
+      pop_basic_mem(code, "@LCL");
+    } else if (strcmp(code->arg_1, "argument") == 0) {
+      pop_basic_mem(code, "@ARG");
+    } else if (strcmp(code->arg_1, "this") == 0) {
+      pop_basic_mem(code, "@THIS");
+    } else if (strcmp(code->arg_1, "that") == 0) {
+      pop_basic_mem(code, "@THAT");
+    } else if (strcmp(code->arg_1, "temp") == 0) {
+      char location[7] = "@Temp";
+      strncat(location, code->arg_2, 2);
+      write_command("@SP M=M-1 A=M D=M\n");
+      fprintf(fp_out, "%s\n", location);
+      write_command("A=M M=D @SP M=M+1\n");
+    }
   }
 }
 
